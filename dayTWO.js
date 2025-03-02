6e6f36c99550a016fd29d7d89664ecbe5389c036
Aldo Flores <sarduts@gmail.com>
2025-03-02 15:24:18 -0600

    organiza el código del dayTWO.js

--- dayTWO.js
+++ dayTWO.js
@@ -9,6 +9,8 @@ console.log(`Hola ${nombre}, tienes ${edad} años y ya estás aprendiendo ${LP}!
 let answer = prompt(`¿Te gusta estudiar ${LP}?  `).toUpperCase();
 let mensaje = "";
 
-answer == 'SI' ? mensaje = "¡Muy bien! Sigue estudiendo y tendrás mucho éxito.": answer == 'NO' ? mensaje = "Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?" : mensaje = "Ingresa una respuesta de 'si/no' a la pregunta anterior.";
+answer == 'SI' ? mensaje = "¡Muy bien! Sigue estudiendo y tendrás mucho éxito.": 
+answer == 'NO' ? mensaje = "Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?" : 
+mensaje = "Ingresa una respuesta de 'si/no' a la pregunta anterior.";
 
 console.log(mensaje);

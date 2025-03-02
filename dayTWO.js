7b79536c5631f2444c9017c95bf350add46f4006
Aldo Flores <sarduts@gmail.com>
2025-03-02 10:22:08 -0600

    agrega código opcional al dayTWO.js

--- dayTWO.js
+++ dayTWO.js
@@ -5,3 +5,10 @@ let edad = prompt("¿Cuántos años tienes?    ");
 let LP = prompt("¿Qué lenguaje de programación estás estudiando?    ");
 
 console.log(`Hola ${nombre}, tienes ${edad} años y ya estás aprendiendo ${LP}!`)
+
+let answer = prompt(`¿Te gusta estudiar ${LP}?  `).toUpperCase();
+let mensaje = "";
+
+answer == 'SI' ? mensaje = "¡Muy bien! Sigue estudiendo y tendrás mucho éxito.": answer == 'NO' ? mensaje = "Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?" : mensaje = "Ingresa una respuesta de 'si/no' a la pregunta anterior.";
+
+console.log(mensaje);

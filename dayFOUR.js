e7a2485132f05f0cfd40cb96014122e31b27cf5b
Aldo Flores <sarduts@gmail.com>
2025-03-04 15:32:19 -0600

    actualiza el dayFOUR.js

--- dayFOUR.js
+++ dayFOUR.js
@@ -1,9 +1,19 @@
 const prompt = require("prompt-sync")();
 
 const numeroSecreto = 5;
-let intentos = 1;
+let intentos = 3;
+let acierto = 0;
 
-while(intentos < 3){
+while(intentos > 0){
 
-    intentos ++;
+    acierto = parseInt(prompt("Adivina el número que estoy pensando (1 - 10):    "));
+    
+    if(acierto === numeroSecreto){
+        console.log(`¡Felicidades!, el número era ${acierto}`);
+        break;
+    }else {
+        intentos --;
+        if(intentos != 0) console.log(`Oh... No era. Tienes ${intentos} ${intentos != 1 ? "intentos": "intento"} más.`);
+    }
+
 }

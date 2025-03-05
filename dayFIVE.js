3fd68ae41b8d3d1ffa77f77d4d925c3bf8878174
Aldo Flores <sarduts@gmail.com>
2025-03-05 16:57:19 -0600

    crea el dayFive.js

---
+++ dayFIVE.js
@@ -0,0 +1,39 @@
+const prompt = require("prompt-sync")();
+
+const categorias = ["verduras", "lacteos", "carnes", "fruta", "congelados", "dulces", "otros"];
+let listaDeCompras = [];
+let verduras = [];
+let lacteos = [];
+let carnes = [];
+let fruta = [];
+let congelados = [];
+let dulces = [];
+let otros = [];
+let entrada = "";
+let categoria = "";
+
+while(true){
+    entrada = prompt("¿Deseas agregar un alimento a tu lista de compras?    ");
+    console.log(`Primera entrada: ${entrada}`);
+    if (entrada.toUpperCase() == 'NO') break;
+    
+    entrada = prompt("¿Qué alimento desear agregar? ");
+    console.log(`Segunda entrada: ${entrada}`);
+
+    categoria = prompt(`¿En que categoría encaja ese alimento? (ingresa el #): ${menu_Categoria()}`);
+    listaDeCompras.push(entrada +":" + categorias[categoria - 1]);
+    console.log(`La lista de compra es: ${listaDeCompras}`);
+
+}
+
+console.log(`Final entrada: ${entrada}`);
+
+function menu_Categoria(){
+    let mensaje = "";
+    for(let i = 0; i < categorias.length; i++){
+        mensaje += `\n  ${i + 1} -  ${categorias[i]}`;
+    }
+    mensaje += "\n"
+    return mensaje;
+}
+

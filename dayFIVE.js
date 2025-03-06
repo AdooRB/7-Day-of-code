e85f130844f4c6badf0734a69d77f7e41c6354f5
Aldo Flores <sarduts@gmail.com>
2025-03-05 21:25:57 -0600

    añada comentarios en dayFIVE.js'

--- dayFIVE.js
+++ dayFIVE.js
@@ -1,23 +1,27 @@
 const prompt = require("prompt-sync")();
-
 const categorias = ["Verduras", "Lacteos", "Carnes", "Fruta", "Congelados", "Dulces", "Otros"];
 let listaDeCompras = [];
 let entrada = "";
 let categoria = "";
 
+//ciclo principal del programa
 while(true){
+    //pregunta para continuar o salir del ciclo principal
     entrada = iniciar_Pregunta(`¿Deseas agregar ${listaDeCompras.length == 0 ? "un" : "otro"} alimento a tu lista de compras?    `);
     if (entrada.toUpperCase() == 'NO') break;
     
+    //2da pregunta, digitalizar el alimento
     entrada = prompt("¿Qué alimento desear agregar? ");
 
+    //3ra pregunta, categorizar el alimento y lo añade a la lista de compras
     categoria = categorizar_Alimento(`¿En que categoría encaja este alimento? (ingresa el #): ${menu_Categoria()}`);
     listaDeCompras.push(entrada +":" + categorias[categoria - 1]);
-    console.log(listaDeCompras);
 }
 
+//mostrar la "Lista de compras" por categorías
 imprimir_Lista();
 
+//enumera las categorias
 function menu_Categoria(){
     let mensaje = "";
     for(let i = 0; i < categorias.length; i++){
@@ -27,6 +31,7 @@ function menu_Categoria(){
     return mensaje;
 }
 
+//pregunta y repite pregunta si la respuesta nos es "SI/NO"
 function iniciar_Pregunta(pregunta){
     let respuesta = prompt(pregunta);
     
@@ -38,6 +43,7 @@ function iniciar_Pregunta(pregunta){
     return respuesta;
 }
 
+//categoriza el alimento dependiendo del # del menu_categorías
 function categorizar_Alimento(pregunta){
     let respuesta = prompt(pregunta);
    
@@ -48,6 +54,7 @@ function categorizar_Alimento(pregunta){
     return respuesta;
 }
 
+//ordena y muestra los datos de la lista de compras
 function imprimir_Lista(){
     console.log("\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
     console.log("LISTA DE COMPRAS:");
@@ -59,8 +66,12 @@ function imprimir_Lista(){
         alimento = `${categorias[i_categoria]}: `;
         
         for (let i_lista = 0; i_lista < listaDeCompras.length; i_lista++) {
+            
             if(listaDeCompras[i_lista].includes(":"+categorias[i_categoria])){
+                
                 alimento == `${categorias[i_categoria]}: ` ? alimento += `  ${listaDeCompras[i_lista].replace(":"+categorias[i_categoria],"")}`: alimento += `, ${listaDeCompras[i_lista].replace(":"+categorias[i_categoria],"")}`;
+                
+            
             }
         }
 

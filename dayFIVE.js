21a899055b0f75bc2e372d50e92d4b72e4deb133
Aldo Flores <sarduts@gmail.com>
2025-03-05 20:31:02 -0600

    agrga la funcion 'categorizar alimento'

--- dayFIVE.js
+++ dayFIVE.js
@@ -1,25 +1,21 @@
 const prompt = require("prompt-sync")();
 
-const categorias = ["verduras", "lacteos", "carnes", "fruta", "congelados", "dulces", "otros"];
+const categorias = ["Verduras", "Lacteos", "Carnes", "Fruta", "Congelados", "Dulces", "Otros"];
 let listaDeCompras = [];
 let entrada = "";
 let categoria = "";
 
 while(true){
-    entrada = iniciar_Pregunta("¿Deseas agregar un alimento a tu lista de compras?    ");
-    console.log(`Primera entrada: ${entrada}`);
+    entrada = iniciar_Pregunta(`¿Deseas agregar ${listaDeCompras.length == 0 ? "un" : "otro"} alimento a tu lista de compras?    `);
     if (entrada.toUpperCase() == 'NO') break;
     
     entrada = prompt("¿Qué alimento desear agregar? ");
-    console.log(`Segunda entrada: ${entrada}`);
 
-    categoria = prompt(`¿En que categoría encaja ese alimento? (ingresa el #): ${menu_Categoria()}`);
+    categoria = categorizar_Alimento(`¿En que categoría encaja este alimento? (ingresa el #): ${menu_Categoria()}`);
     listaDeCompras.push(entrada +":" + categorias[categoria - 1]);
-    console.log(`La lista de compra es: ${listaDeCompras}`);
-
+    console.log(listaDeCompras);
 }
 
-console.log(`Final entrada: ${entrada}`);
 imprimir_Lista();
 
 function menu_Categoria(){
@@ -42,8 +38,19 @@ function iniciar_Pregunta(pregunta){
     return respuesta;
 }
 
+function categorizar_Alimento(pregunta){
+    let respuesta = prompt(pregunta);
+   
+    while(parseInt(respuesta) > categorias.length || isNaN(parseInt(respuesta))){
+        console.log("Valor incorrecto. Digitaliza el numeral de la categoría.");
+        respuesta = prompt(pregunta);
+    }
+    return respuesta;
+}
+
 function imprimir_Lista(){
-    console.log("Lista de compras:");
+    console.log("\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+    console.log("LISTA DE COMPRAS:");
     
     let alimento = "";
 
@@ -53,12 +60,13 @@ function imprimir_Lista(){
         
         for (let i_lista = 0; i_lista < listaDeCompras.length; i_lista++) {
             if(listaDeCompras[i_lista].includes(":"+categorias[i_categoria])){
-                alimento == `${categorias[i_categoria]}: ` ? alimento += `${listaDeCompras[i_lista].replace(":"+categorias[i_categoria],"")}`: alimento += `, ${listaDeCompras[i_lista].replace(":"+categorias[i_categoria],"")}`;
+                alimento == `${categorias[i_categoria]}: ` ? alimento += `  ${listaDeCompras[i_lista].replace(":"+categorias[i_categoria],"")}`: alimento += `, ${listaDeCompras[i_lista].replace(":"+categorias[i_categoria],"")}`;
             }
         }
 
-        alimento == `${categorias[i_categoria]}: ` ? alimento = "": console.log(alimento);
+        alimento == `${categorias[i_categoria]}: ` ? alimento = "": console.log(`   ${alimento}`);
     }
+    console.log("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
     return;
 }
 
